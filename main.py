e11863da8e50c15d5024ac1faf4257eb4e3c8a11
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-11-17 17:46:07 -0300

    Integrando com o via cep

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from search_address_brazil import SearchAddressBrazil
 
-cep = 25870145
+cep = 65057869
 search_address_brazil = SearchAddressBrazil(cep)
 print(search_address_brazil)
+response = search_address_brazil.get_address_by_via_cep()
+print(response)
--- search_address_brazil.py
+++ search_address_brazil.py
@@ -1,3 +1,6 @@
+import requests
+
+
 class SearchAddressBrazil:
 
     def __init__(self, cep):
@@ -11,6 +14,17 @@ class SearchAddressBrazil:
 
     def formated(self):
         return "{}-{}".format(self.__cep[:5], self.__cep[5:])
+
+    def get_address_by_via_cep(self):
+        url_request = "https://viacep.com.br/ws/{}/json/".format(self.__cep)
+        request_get = requests.get(url_request)
+        address = request_get.json()
+
+        return [
+            address["bairro"],
+            address["localidade"],
+            address["uf"]
+        ]
 
     def __str__(self):
         return self.formated()
